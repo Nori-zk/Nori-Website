5a97b95386db1c66f0116c8733568cf862c2a23e
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-11-17 09:37:41 +0000

    feat: Mina coin

--- src/components/Swap.tsx
+++ src/components/Swap.tsx
@@ -1,11 +1,24 @@
 import EthereumIcon from "../assets/Ethereum_Icon.png";
+import MinaIcon from "../assets/Mina_Icon.png";
 import Arrow from "../assets/Arrow.svg?react";
 
 const Swap = () => {
   return (
-    <div className="grid grid-cols-2 grid-rows-2 gap-1 mx-auto scale-50 sm:scale-75 md:scale-90 lg:scale-100" style={{ width: '400px', height: '400px' }}>
+    <div
+      className="grid grid-cols-2 grid-rows-2 gap-1 mx-auto scale-50 sm:scale-75 md:scale-90 lg:scale-100"
+      style={{ width: "400px", height: "400px" }}
+    >
       <div className="flex justify-center items-center -ml-16 lg:-ml-32 w-full h-full">
-        <img src={EthereumIcon} className="flex-shrink-0 object-cover" style={{ width: '208px', height: '208px', minWidth: '208px', minHeight: '208px' }} />
+        <img
+          src={MinaIcon}
+          className="flex-shrink-0 object-cover"
+          style={{
+            width: "275px",
+            height: "275px",
+            minWidth: "275px",
+            minHeight: "275px",
+          }}
+        />
       </div>
       <div className="flex justify-center items-center -ml-16 lg:-ml-32 w-full h-full">
         <Arrow className="w-40 rotate-[170deg] flex-shrink-0" />
@@ -14,7 +27,16 @@ const Swap = () => {
         <Arrow className="w-40 rotate-[360deg] flex-shrink-0" />
       </div>
       <div className="flex justify-center items-center w-full h-full">
-        <img src={EthereumIcon} className="flex-shrink-0 object-cover" style={{ width: '208px', height: '208px', minWidth: '208px', minHeight: '208px' }} />
+        <img
+          src={EthereumIcon}
+          className="flex-shrink-0 object-cover"
+          style={{
+            width: "208px",
+            height: "208px",
+            minWidth: "208px",
+            minHeight: "208px",
+          }}
+        />
       </div>
     </div>
   );
